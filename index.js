78ab1673c4cf65c42da339828fdeed9903aa9f8a
charles Gift <charlesgiftangila@gmail.com>
2024-07-18 20:39:46 +0300

    updates lab

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  const fetchedUrl = fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((book) => renderBooks(book));
+  return fetchedUrl;
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
 });
